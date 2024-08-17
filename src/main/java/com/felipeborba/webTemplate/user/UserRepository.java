bd081787e02755192e5f271dcd117321216d578d
Felipe Borba <felipe.borba0109@gmail.com>
2024-08-17 19:55:56 -0300

    clean files and add postgress dependency

--- src/main/java/com/felipeborba/webTemplate/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.felipeborba.webTemplate.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-    User findByLogin(String login);
-}
